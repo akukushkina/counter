bde7e48c8215de4be3706b0a567368b448f1da52
akyukyushkina <akyukyushkina@gmail.com>
2022-10-24 16:17:33 +0100

    add js

--- main.js
+++ main.js
@@ -1,21 +1,43 @@
-const counter = document.querySelector('.counter');
-const minus = document.querySelector('.minus');
-const reset = document.querySelector('.reset');
-const plus = document.querySelector('.plus');
+const counter = document.querySelector(".counter");
+let count = 0;
 
-// counter.textContent = 0;
+// add eventListener to each button
 
-minus.addEventListener('click', function (){
-    counter.textContent--;
-})
+// const minus = document.querySelector('.minus');
+// const reset = document.querySelector('.reset');
+// const plus = document.querySelector('.plus');
 
-reset.addEventListener('click', function(){
-    counter.textContent = 0;
-})
+// minus.addEventListener('click', function (){
+//     counter.textContent--;
+// })
 
-plus.addEventListener('click', function(){
-    counter.textContent++;
-})
+// reset.addEventListener('click', function(){
+//     counter.textContent = 0;
+// })
 
+// plus.addEventListener('click', function(){
+//     counter.textContent++;
+// })
 
+// add event listener to all buttons in one time
 
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    let styles = e.currentTarget.classList;
+    styles.contains("minus")
+      ? count--
+      : styles.contains("plus")
+      ? count++
+      : (count = 0);
+
+    counter.textContent = count;
+
+    count > 0
+      ? (counter.style.color = "green")
+      : count < 0
+      ? (counter.style.color = "red")
+      : (counter.style.color = "black");
+  });
+});
